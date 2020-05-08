c58ca64a4c04d4583869a7eaafa12403d7b4f90a
serhikozak <skozakov924@gmail.com>
2020-05-08 13:08:54 +0200

    Add method append

--- L3Task1.py
+++ L3Task1.py
@@ -1,14 +1,18 @@
 # Create of list random number and define even number
+# if that list haven't any even number we get an appropriate print('In that list, not have even number')
 import random
 
 n = int(input('Input any number '))
 my_list = [random.randint(0, 88) for i in range(n)]
 print(my_list)
+my_list_even_number = []
 for my_list in my_list:
     if my_list % 2 == 0:
-        print(my_list)
-    elif my_list % 2 == 1:
-        print('In that list, not have even number')
-    # else:
-    #     my_list % 2 == 1
-    # print('In that list, not have even number')
+        my_list_even_number.append(my_list)
+
+if len(my_list_even_number) > 0:
+    print(my_list_even_number)
+else:
+    print('In that list, not have even number')
+
+
