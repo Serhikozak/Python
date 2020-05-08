6730b4e899ec781e015331a717296f7b5ae60bac
serhikozak <skozakov924@gmail.com>
2020-05-08 16:08:31 +0200

    First commit

---
+++ L3Task2.py
@@ -0,0 +1,15 @@
+# Sum of random List
+import random
+
+
+def sum_my_list(s):
+    return sum(s)
+
+
+quantity_number = int(input('Input any number '))
+my_list = [random.randint(0, 10) for i in range(quantity_number)]
+print(my_list)
+sum_my_list(my_list)
+print(sum(my_list))
+
+
